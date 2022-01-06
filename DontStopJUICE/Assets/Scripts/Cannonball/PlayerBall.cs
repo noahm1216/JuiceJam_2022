5f6b24385ab35b82f8d37ce1ec87d8f9cd4f22e5
Noah <noahm1216@gmail.com>
2022-01-05 23:24:44 -0800

    Cannon Scene (not working yet)

---
+++ DontStopJUICE/Assets/Scripts/Cannonball/PlayerBall.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    public Rigidbody rbBall;
+    public float speedMove;
+    public Transform tranCam;
+
+    public Transform currentCannon;
+    public float speedRotate;
+    public bool inCannon;
+    public float powerCannon;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!inCannon)
+        {
+            //movement inputs
+            if (Input.GetKey("w"))
+            {
+                rbBall.AddForce(tranCam.forward * speedMove * Time.deltaTime);
+            }
+            if (Input.GetKey("s"))
+            {
+                rbBall.AddForce(tranCam.forward * -speedMove * Time.deltaTime);
+            }
+            if (Input.GetKey("d"))
+            {
+                rbBall.AddForce(tranCam.right * speedMove * Time.deltaTime);
+            }
+            if (Input.GetKey("a"))
+            {
+                rbBall.AddForce(tranCam.right * -speedMove * Time.deltaTime);
+            }
+        }//end not in cannon
+        else
+        //else in a cannon
+        {
+            //cannon rotations            
+            if (Input.GetKey("d"))
+            {
+                currentCannon.transform.Rotate(0, -speedRotate * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("a"))
+            {
+                currentCannon.transform.Rotate(0, speedRotate * Time.deltaTime, 0);
+            }
+
+            //cannon shoot
+            if (Input.GetKey("space"))
+            {
+                print("launch");
+                rbBall.isKinematic = false;
+                rbBall.AddForce(currentCannon.forward * powerCannon);
+                inCannon = false;
+            }
+        }
+
+    }//end update
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if(col.transform.tag == "Cannon")
+        {            
+            currentCannon = col.transform.GetChild(0) ;
+            rbBall.isKinematic = true;
+            transform.position = currentCannon.position;
+            inCannon = true;
+            col.transform.tag = "Untagged";
+        }
+        
+    }//end of collision enter
+
+
+}//end of playerball script
