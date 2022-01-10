a878c5a802122f4bc444e4b21b33b4ba0162cf29
Noah <noahm1216@gmail.com>
2022-01-09 16:30:25 -0800

    We didnt get very far =[

--- DontStopJUICE/Assets/Scripts/Cannonball/PlayerBall.cs
+++ DontStopJUICE/Assets/Scripts/Cannonball/PlayerBall.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
+    public bool justMovement;
     public Rigidbody rbBall;
     public float speedMove;
     public Transform tranCam;
@@ -18,8 +19,7 @@ public class PlayerBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (!inCannon)
+        if (justMovement)
         {
             //movement inputs
             if (Input.GetKey("w"))
@@ -38,31 +38,57 @@ public class PlayerBall : MonoBehaviour
             {
                 rbBall.AddForce(tranCam.right * -speedMove * Time.deltaTime);
             }
-        }//end not in cannon
+        }
+        //if the cannonball one
         else
-        //else in a cannon
         {
-            //cannon rotations            
-            if (Input.GetKey("d"))
-            {
-                currentCannon.transform.Rotate(speedRotate * Time.deltaTime, 0, 0);
-            }
-            if (Input.GetKey("a"))
-            {
-                currentCannon.transform.Rotate(-speedRotate * Time.deltaTime, 0, 0);
-            }
 
-            //cannon shoot
-            if (Input.GetKey("space"))
+
+            if (!inCannon)
             {
-                print("launch");
-                transform.position = currentCannon.GetChild(0).position;
-                rbBall.isKinematic = false;
-                rbBall.AddForce(currentCannon.forward * powerCannon);
-                //rbBall.AddRelativeForce(currentCannon.forward * powerCannon);
-                inCannon = false;
-                currentCannon.parent.tag = "Cannon";
-                currentCannon = null;
+                //movement inputs
+                if (Input.GetKey("w"))
+                {
+                    rbBall.AddForce(tranCam.forward * speedMove * Time.deltaTime);
+                }
+                if (Input.GetKey("s"))
+                {
+                    rbBall.AddForce(tranCam.forward * -speedMove * Time.deltaTime);
+                }
+                if (Input.GetKey("d"))
+                {
+                    rbBall.AddForce(tranCam.right * speedMove * Time.deltaTime);
+                }
+                if (Input.GetKey("a"))
+                {
+                    rbBall.AddForce(tranCam.right * -speedMove * Time.deltaTime);
+                }
+            }//end not in cannon
+            else
+            //else in a cannon
+            {
+                //cannon rotations            
+                if (Input.GetKey("d"))
+                {
+                    currentCannon.transform.Rotate(speedRotate * Time.deltaTime, 0, 0);
+                }
+                if (Input.GetKey("a"))
+                {
+                    currentCannon.transform.Rotate(-speedRotate * Time.deltaTime, 0, 0);
+                }
+
+                //cannon shoot
+                if (Input.GetKey("space"))
+                {
+                    print("launch");
+                    transform.position = currentCannon.GetChild(0).position;
+                    rbBall.isKinematic = false;
+                    rbBall.AddForce(currentCannon.forward * powerCannon);
+                    //rbBall.AddRelativeForce(currentCannon.forward * powerCannon);
+                    inCannon = false;
+                    currentCannon.parent.tag = "Cannon";
+                    currentCannon = null;
+                }
             }
         }
 
@@ -71,35 +97,49 @@ public class PlayerBall : MonoBehaviour
 
     private void OnCollisionEnter(Collision col)
     {
+        if (!justMovement)
+        {
 
-        if(col.transform.tag == "Cannon")
-        {            
-            currentCannon = col.transform.GetChild(0) ;
-            rbBall.isKinematic = true;
-            transform.position = currentCannon.position;
-            inCannon = true;
-            col.transform.tag = "Untagged";
+
+
+            if (col.transform.tag == "Cannon")
+            {
+                currentCannon = col.transform.GetChild(0);
+                rbBall.isKinematic = true;
+                transform.position = currentCannon.position;
+                inCannon = true;
+                col.transform.tag = "Untagged";
+            }
         }
-        
     }//end of collision enter
 
     //enter
     private void OnTriggerEnter(Collider trig)
     {
-        if(trig.tag == "ChangeCamera")
+        if (!justMovement)
         {
-            camView.SetActive(false);
-            camScore.SetActive(true);
+
+
+            if (trig.tag == "ChangeCamera")
+            {
+                camView.SetActive(false);
+                camScore.SetActive(true);
+            }
         }
     }//trigger enter
 
     //exit
     private void OnTriggerExit(Collider trig)
     {
-        if (trig.tag == "ChangeCamera")
+        if (!justMovement)
         {
-            camView.SetActive(true);
-            camScore.SetActive(false);
+
+
+            if (trig.tag == "ChangeCamera")
+            {
+                camView.SetActive(true);
+                camScore.SetActive(false);
+            }
         }
     }//trigger exit
 
